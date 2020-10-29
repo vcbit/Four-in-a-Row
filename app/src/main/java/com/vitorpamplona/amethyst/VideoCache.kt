0b5868c5f7c11e1fed1ec23632473a1a0948a88e
vcbit <118248899+vcbit@users.noreply.github.com>
2020-10-29 16:06:26 +0000

    latest versions

--- app/src/main/java/com/vitorpamplona/amethyst/VideoCache.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/VideoCache.kt
@@ -3,4 +3,17 @@ package com.vitorpamplona.amethyst
 import android.content.Context
 import com.google.android.exoplayer2.database.StandaloneDatabaseProvider
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
-import com.google.android.exoplayer2.u
+import com.google.android.exoplayer2.upstream.cache.CacheDataSource
+import com.google.android.exoplayer2.upstream.cache.LeastRecentlyUsedCacheEvictor
+import com.google.android.exoplayer2.upstream.cache.SimpleCache
+
+object VideoCache {
+
+    var exoPlayerCacheSize: Long = 90 * 1024 * 1024 // 90MB
+
+    var leastRecentlyUsedCacheEvictor = LeastRecentlyUsedCacheEvictor(exoPlayerCacheSize)
+
+    lateinit var exoDatabaseProvider: StandaloneDatabaseProvider
+    lateinit var simpleCache: SimpleCache
+
+    lateinit var cacheD
