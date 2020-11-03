3e887336b230d472410b26be8cefe429c297fa94
vcbit <118248899+vcbit@users.noreply.github.com>
2020-11-03 12:06:32 +0000

    powerpc/mm/numa: Fix break placement

--- app/src/main/java/com/vitorpamplona/amethyst/model/Account.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/model/Account.kt
@@ -2,4 +2,11 @@ package com.vitorpamplona.amethyst.model
 
 import android.content.res.Resources
 import androidx.core.os.ConfigurationCompat
-import androidx.lifecycle.LiveD
+import androidx.lifecycle.LiveData
+import com.vitorpamplona.amethyst.service.model.ChannelCreateEvent
+import com.vitorpamplona.amethyst.service.model.ChannelMessageEvent
+import com.vitorpamplona.amethyst.service.model.ChannelMetadataEvent
+import com.vitorpamplona.amethyst.service.model.Contact
+import com.vitorpamplona.amethyst.service.model.ContactListEvent
+import com.vitorpamplona.amethyst.service.model.DeletionEvent
+import com.vitorpamplona.amethyst.service.model.Iden
