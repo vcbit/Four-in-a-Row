0cbe8554295f4bf2942266af313f2466eff7e44c
vcbit <118248899+vcbit@users.noreply.github.com>
2020-12-26 11:42:18 +0000

    v3.0.0-beta.2

--- app/src/main/java/com/vitorpamplona/amethyst/model/AntiSpamFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/model/AntiSpamFilter.kt
@@ -4,3 +4,19 @@ import android.util.Log
 import android.util.LruCache
 import androidx.lifecycle.LiveData
 import com.vitorpamplona.amethyst.service.model.Event
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.atomic.AtomicBoolean
+
+data class Spammer(val pubkeyHex: HexKey, var duplicatedMessages: Set<HexKey>)
+
+class AntiSpamFilter {
+    val recentMessages = LruCache<Int, String>(1000)
+    val spamMessages = LruCache<Int, Spammer>(1000)
+
+    @Synchro
