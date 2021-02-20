3bafbe3ccbd3c52a1fedc57b4d0673a491a74e0b
vcbit <118248899+vcbit@users.noreply.github.com>
2021-02-20 11:54:12 +0000

    main.js

--- app/src/main/java/com/vitorpamplona/amethyst/model/UrlCachedPreviewer.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/model/UrlCachedPreviewer.kt
@@ -4,4 +4,18 @@ import com.baha.url.preview.BahaUrlPreview
 import com.baha.url.preview.IUrlPreviewCallback
 import com.baha.url.preview.UrlInfoItem
 import com.vitorpamplona.amethyst.ui.components.imageExtension
-import com.vitorpamplona.amethyst.ui.components.isVal
+import com.vitorpamplona.amethyst.ui.components.isValidURL
+import com.vitorpamplona.amethyst.ui.components.noProtocolUrlValidator
+import com.vitorpamplona.amethyst.ui.components.videoExtension
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+object UrlCachedPreviewer {
+    var cache = mapOf<String, UrlInfoItem>()
+        private set
+    var failures = mapOf<String, Throwable>()
+        private set
+
+    fun previewInfo(url: String, callba
