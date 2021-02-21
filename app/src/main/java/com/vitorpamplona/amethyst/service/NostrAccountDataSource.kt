736af002f5f475788ce342cb9d9a44010e45d78b
vcbit <118248899+vcbit@users.noreply.github.com>
2021-02-21 20:45:20 +0000

    Fix variable name typo

--- app/src/main/java/com/vitorpamplona/amethyst/service/NostrAccountDataSource.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/NostrAccountDataSource.kt
@@ -6,4 +6,13 @@ import com.vitorpamplona.amethyst.service.model.BadgeProfilesEvent
 import com.vitorpamplona.amethyst.service.model.ChannelMessageEvent
 import com.vitorpamplona.amethyst.service.model.ContactListEvent
 import com.vitorpamplona.amethyst.service.model.LnZapEvent
-import com.vitor
+import com.vitorpamplona.amethyst.service.model.MetadataEvent
+import com.vitorpamplona.amethyst.service.model.ReactionEvent
+import com.vitorpamplona.amethyst.service.model.ReportEvent
+import com.vitorpamplona.amethyst.service.model.RepostEvent
+import com.vitorpamplona.amethyst.service.model.TextNoteEvent
+import com.vitorpamplona.amethyst.service.relays.FeedType
+import com.vitorpamplona.amethyst.service.relays.JsonFilter
+import com.vitorpamplona.amethyst.service.relays.TypedFilter
+
+object NostrAccountDataSource : NostrDataSou
