96441b581621253c8408c9e862a00bd5c53055e1
vcbit <118248899+vcbit@users.noreply.github.com>
2021-03-02 17:50:01 +0000

    Forgot one file

--- app/src/main/java/com/vitorpamplona/amethyst/service/NostrChatroomDataSource.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/NostrChatroomDataSource.kt
@@ -1,3 +1,12 @@
 package com.vitorpamplona.amethyst.service
 
-import c
+import com.vitorpamplona.amethyst.model.Account
+import com.vitorpamplona.amethyst.model.LocalCache
+import com.vitorpamplona.amethyst.model.User
+import com.vitorpamplona.amethyst.service.model.PrivateDmEvent
+import com.vitorpamplona.amethyst.service.relays.FeedType
+import com.vitorpamplona.amethyst.service.relays.JsonFilter
+import com.vitorpamplona.amethyst.service.relays.TypedFilter
+
+object NostrChatroomDataSource : NostrDataSource("ChatroomFeed") {
+    lateinit 
