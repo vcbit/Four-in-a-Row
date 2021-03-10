2c4d8219cdf86bde7689b14f5446b53cf1d04c39
vcbit <118248899+vcbit@users.noreply.github.com>
2021-03-10 17:35:47 +0000

    Coding style cleanups.

--- app/src/main/java/com/vitorpamplona/amethyst/service/NostrSingleChannelDataSource.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/NostrSingleChannelDataSource.kt
@@ -3,4 +3,18 @@ package com.vitorpamplona.amethyst.service
 import com.vitorpamplona.amethyst.model.LocalCache
 import com.vitorpamplona.amethyst.service.model.ChannelCreateEvent
 import com.vitorpamplona.amethyst.service.model.ChannelMetadataEvent
-import com.vitorpamplona.amethyst.service.relays
+import com.vitorpamplona.amethyst.service.relays.FeedType
+import com.vitorpamplona.amethyst.service.relays.JsonFilter
+import com.vitorpamplona.amethyst.service.relays.TypedFilter
+
+object NostrSingleChannelDataSource : NostrDataSource("SingleChannelFeed") {
+    private var channelsToWatch = setOf<String>()
+
+    private fun createRepliesAndReactionsFilter(): TypedFilter? {
+        val reactionsToWatch = channelsToWatch.map { it }
+
+        if (reactionsToWatch.isEmpty()) {
+            return null
+        }
+
+        // 
