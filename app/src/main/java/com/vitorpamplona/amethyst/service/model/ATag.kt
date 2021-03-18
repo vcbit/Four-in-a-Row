140a61141fd9931d5db16eb997a5e6b6630fb817
vcbit <118248899+vcbit@users.noreply.github.com>
2021-03-18 11:57:34 +0000

    Clean build

--- app/src/main/java/com/vitorpamplona/amethyst/service/model/ATag.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/model/ATag.kt
@@ -4,4 +4,17 @@ import android.util.Log
 import com.vitorpamplona.amethyst.model.toByteArray
 import com.vitorpamplona.amethyst.model.toHexKey
 import com.vitorpamplona.amethyst.service.nip19.Tlv
-import fr.
+import fr.acinq.secp256k1.Hex
+import nostr.postr.Bech32
+import nostr.postr.bechToBytes
+import nostr.postr.toByteArray
+
+data class ATag(val kind: Int, val pubKeyHex: String, val dTag: String, val relay: String?) {
+    fun toTag() = "$kind:$pubKeyHex:$dTag"
+
+    fun toNAddr(): String {
+        val kind = kind.toByteArray()
+        val author = pubKeyHex.toByteArray()
+        val dTag = dTag.toByteArray(Charsets.UTF_8)
+        val relay = relay?.toByteArray(Charsets.UTF_8)
+
