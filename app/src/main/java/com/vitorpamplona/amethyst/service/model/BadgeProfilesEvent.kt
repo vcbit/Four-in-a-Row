8bfdf81d156dd3a5a64dc5358d1dd3052cb2a778
vcbit <118248899+vcbit@users.noreply.github.com>
2021-03-18 17:25:11 +0000

    add titles

--- app/src/main/java/com/vitorpamplona/amethyst/service/model/BadgeProfilesEvent.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/model/BadgeProfilesEvent.kt
@@ -6,3 +6,16 @@ class BadgeProfilesEvent(
     id: HexKey,
     pubKey: HexKey,
     createdAt: Long,
+    tags: List<List<String>>,
+    content: String,
+    sig: HexKey
+) : Event(id, pubKey, createdAt, kind, tags, content, sig) {
+    fun badgeAwardEvents() = tags.filter { it.firstOrNull() == "e" }.mapNotNull { it.getOrNull(1) }
+    fun badgeAwardDefinitions() = tags.filter { it.firstOrNull() == "a" }.mapNotNull {
+        val aTagValue = it.getOrNull(1)
+        val relay = it.getOrNull(2)
+
+        if (aTagValue != null) ATag.parse(aTagValue, relay) else null
+    }
+
+    fun 
