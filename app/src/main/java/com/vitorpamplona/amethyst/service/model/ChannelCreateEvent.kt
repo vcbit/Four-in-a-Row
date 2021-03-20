a581a3a042074bd3a31027619f9842d96f5ef20d
vcbit <118248899+vcbit@users.noreply.github.com>
2021-03-20 15:19:10 +0000

    Update docstrings.

--- app/src/main/java/com/vitorpamplona/amethyst/service/model/ChannelCreateEvent.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/model/ChannelCreateEvent.kt
@@ -8,4 +8,20 @@ import java.util.Date
 
 class ChannelCreateEvent(
     id: HexKey,
-    pubKey: He
+    pubKey: HexKey,
+    createdAt: Long,
+    tags: List<List<String>>,
+    content: String,
+    sig: HexKey
+) : Event(id, pubKey, createdAt, kind, tags, content, sig) {
+    fun channelInfo() = try {
+        MetadataEvent.gson.fromJson(content, ChannelData::class.java)
+    } catch (e: Exception) {
+        Log.e("ChannelMetadataEvent", "Can't parse channel info $content", e)
+        ChannelData(null, null, null)
+    }
+
+    companion object {
+        const val kind = 40
+
+        fun create(channelInfo: ChannelData?, privateKey: ByteArray, createdAt:
