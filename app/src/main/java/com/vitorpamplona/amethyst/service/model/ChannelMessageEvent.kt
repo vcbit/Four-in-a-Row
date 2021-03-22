ffb8a4a3e6d47da0d107c476cb040c66fe6e8259
vcbit <118248899+vcbit@users.noreply.github.com>
2021-03-22 10:10:45 +0000

    updates description

--- app/src/main/java/com/vitorpamplona/amethyst/service/model/ChannelMessageEvent.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/model/ChannelMessageEvent.kt
@@ -1,3 +1,17 @@
 package com.vitorpamplona.amethyst.service.model
 
-import
+import com.vitorpamplona.amethyst.model.HexKey
+import com.vitorpamplona.amethyst.model.toHexKey
+import nostr.postr.Utils
+import java.util.Date
+
+class ChannelMessageEvent(
+    id: HexKey,
+    pubKey: HexKey,
+    createdAt: Long,
+    tags: List<List<String>>,
+    content: String,
+    sig: HexKey
+) : Event(id, pubKey, createdAt, kind, tags, content, sig) {
+
+    fun channel() = tags.firstOrNull { it[0] == "e" && it.size > 3 && it[3] == "root" }?.getOrNull(1) ?: tags.firstOrNull { it.f
