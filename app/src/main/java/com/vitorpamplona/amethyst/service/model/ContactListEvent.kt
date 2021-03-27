05a203e1c2ea246d7d593eeb90019591774a0f22
vcbit <118248899+vcbit@users.noreply.github.com>
2021-03-27 05:48:47 +0000

    Reviewed by Oliver Hunt.

--- app/src/main/java/com/vitorpamplona/amethyst/service/model/ContactListEvent.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/model/ContactListEvent.kt
@@ -5,4 +5,21 @@ import com.google.gson.reflect.TypeToken
 import com.vitorpamplona.amethyst.model.HexKey
 import com.vitorpamplona.amethyst.model.toHexKey
 import nostr.postr.Utils
-import java
+import java.util.Date
+
+data class Contact(val pubKeyHex: String, val relayUri: String?)
+
+class ContactListEvent(
+    id: HexKey,
+    pubKey: HexKey,
+    createdAt: Long,
+    tags: List<List<String>>,
+    content: String,
+    sig: HexKey
+) : Event(id, pubKey, createdAt, kind, tags, content, sig) {
+    fun follows() = try {
+        tags.filter { it[0] == "p" }.map { Contact(it[1], it.getOrNull(2)) }
+    } catch (e: Exception) {
+        Log.e("ContactListEvent", "can't parse tags as follows: $tags", e)
+        null
+ 
