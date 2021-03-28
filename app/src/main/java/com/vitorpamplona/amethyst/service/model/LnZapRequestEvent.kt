4fed03eb98996167859059f233b4eb9f67f1477a
vcbit <118248899+vcbit@users.noreply.github.com>
2021-03-28 17:24:12 +0000

    msm: Improve qpnp-vibrator scheduling

--- app/src/main/java/com/vitorpamplona/amethyst/service/model/LnZapRequestEvent.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/model/LnZapRequestEvent.kt
@@ -3,4 +3,17 @@ package com.vitorpamplona.amethyst.service.model
 import com.vitorpamplona.amethyst.model.HexKey
 import com.vitorpamplona.amethyst.model.toHexKey
 import nostr.postr.Utils
-import java.ut
+import java.util.Date
+
+class LnZapRequestEvent(
+    id: HexKey,
+    pubKey: HexKey,
+    createdAt: Long,
+    tags: List<List<String>>,
+    content: String,
+    sig: HexKey
+) : Event(id, pubKey, createdAt, kind, tags, content, sig) {
+    fun zappedPost() = tags.filter { it.firstOrNull() == "e" }.mapNotNull { it.getOrNull(1) }
+    fun zappedAuthor() = tags.filter { it.firstOrNull() == "p" }.mapNotNull { it.getOrNull(1) }
+    fun taggedAddresses() = tags.filter { it.firstOrNull() == "a" }.mapNotNull {
+        va
