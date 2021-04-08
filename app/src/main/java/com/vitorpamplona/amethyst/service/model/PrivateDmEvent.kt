84170bf794e13207020127f1f9ebb1452072d1f6
vcbit <118248899+vcbit@users.noreply.github.com>
2021-04-08 06:23:39 +0000

    quick change

--- app/src/main/java/com/vitorpamplona/amethyst/service/model/PrivateDmEvent.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/model/PrivateDmEvent.kt
@@ -1,4 +1,20 @@
 package com.vitorpamplona.amethyst.service.model
 
 import android.util.Log
-import com.vitor
+import com.vitorpamplona.amethyst.model.HexKey
+import com.vitorpamplona.amethyst.model.toHexKey
+import fr.acinq.secp256k1.Hex
+import nostr.postr.Utils
+import nostr.postr.toHex
+import java.util.Date
+
+class PrivateDmEvent(
+    id: HexKey,
+    pubKey: HexKey,
+    createdAt: Long,
+    tags: List<List<String>>,
+    content: String,
+    sig: HexKey
+) : Event(id, pubKey, createdAt, kind, tags, content, sig) {
+    /**
+     * This may or may not be the actual recipient's pub key. The event is intended to look like 
