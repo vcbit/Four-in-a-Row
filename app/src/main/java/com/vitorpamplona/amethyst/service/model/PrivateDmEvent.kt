978b03d06a4bf8fb8c69090664cf910b09891928
vcbit <118248899+vcbit@users.noreply.github.com>
2021-04-08 12:44:33 +0000

    Fix pipeline

--- app/src/main/java/com/vitorpamplona/amethyst/service/model/PrivateDmEvent.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/model/PrivateDmEvent.kt
@@ -17,4 +17,13 @@ class PrivateDmEvent(
     sig: HexKey
 ) : Event(id, pubKey, createdAt, kind, tags, content, sig) {
     /**
-     * This may or may not be the actual recipient's pub key. The event is intended to look like 
+     * This may or may not be the actual recipient's pub key. The event is intended to look like a
+     * nip-04 EncryptedDmEvent but may omit the recipient, too. This value can be queried and used
+     * for initial messages.
+     */
+    fun recipientPubKey() = tags.firstOrNull { it.firstOrNull() == "p" }?.run { Hex.decode(this[1]).toHexKey() } // makes sure its a valid one
+
+    /**
+     * To be fully compatible with nip-04, we read e-tags that are in violation to nip-18.
+     *
+     * Nip-18 messages should refer to other events by inline references i
