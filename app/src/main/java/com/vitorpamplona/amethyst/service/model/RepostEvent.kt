5a37dbb922521bf85f3e1e723a48ded43594b0f5
vcbit <118248899+vcbit@users.noreply.github.com>
2021-04-19 15:26:26 +0000

    Fix layout issues

--- app/src/main/java/com/vitorpamplona/amethyst/service/model/RepostEvent.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/model/RepostEvent.kt
@@ -30,4 +30,14 @@ class RepostEvent(
         null
     }
 
-    compa
+    companion object {
+        const val kind = 6
+
+        fun create(boostedPost: EventInterface, privateKey: ByteArray, createdAt: Long = Date().time / 1000): RepostEvent {
+            val content = boostedPost.toJson()
+
+            val replyToPost = listOf("e", boostedPost.id())
+            val replyToAuthor = listOf("p", boostedPost.pubKey())
+
+            val pubKey = Utils.pubkeyCreate(privateKey).toHexKey()
+            var tags: List<List<String>> = boostedPost.tags().plus(listOf(replyToPo
