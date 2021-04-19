0292c1a7eccbec3e28e2c9060b456e15b724d55a
vcbit <118248899+vcbit@users.noreply.github.com>
2021-04-19 09:07:14 +0000

    Updated the xsimd feedstock.

--- app/src/main/java/com/vitorpamplona/amethyst/service/model/RepostEvent.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/model/RepostEvent.kt
@@ -16,4 +16,18 @@ class RepostEvent(
 ) : Event(id, pubKey, createdAt, kind, tags, content, sig) {
 
     fun boostedPost() = tags.filter { it.firstOrNull() == "e" }.mapNotNull { it.getOrNull(1) }
-  
+    fun originalAuthor() = tags.filter { it.firstOrNull() == "p" }.mapNotNull { it.getOrNull(1) }
+    fun taggedAddresses() = tags.filter { it.firstOrNull() == "a" }.mapNotNull {
+        val aTagValue = it.getOrNull(1)
+        val relay = it.getOrNull(2)
+
+        if (aTagValue != null) ATag.parse(aTagValue, relay) else null
+    }
+
+    fun containedPost() = try {
+        fromJson(content, Client.lenient)
+    } catch (e: Exception) {
+        null
+    }
+
+    compa
