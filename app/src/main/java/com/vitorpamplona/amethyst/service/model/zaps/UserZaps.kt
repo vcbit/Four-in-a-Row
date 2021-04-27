f737de7eb3664aa9a2defc1dd70c873055fb26be
vcbit <118248899+vcbit@users.noreply.github.com>
2021-04-27 06:44:29 +0000

    ext3: fix data=journal fast mount/umount hang

--- app/src/main/java/com/vitorpamplona/amethyst/service/model/zaps/UserZaps.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/model/zaps/UserZaps.kt
@@ -1,3 +1,16 @@
 package com.vitorpamplona.amethyst.service.model.zaps
 
-import com.
+import com.vitorpamplona.amethyst.model.Note
+import com.vitorpamplona.amethyst.service.model.LnZapEventInterface
+
+object UserZaps {
+    fun forProfileFeed(zaps: Map<Note, Note?>?): List<Pair<Note, Note>> {
+        if (zaps == null) return emptyList()
+
+        return (
+            zaps
+                .filter { it.value != null }
+                .toList()
+                .sortedBy { (it.second?.event as? LnZapEventInterface)?.amount() }
+                .reversed()
+   
