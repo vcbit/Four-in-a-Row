efd8a88df300d66422b52f20a7688000465a5ca2
vcbit <118248899+vcbit@users.noreply.github.com>
2021-04-30 13:34:41 +0000

    Small correction

--- app/src/main/java/com/vitorpamplona/amethyst/service/nip19/Tlv.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/nip19/Tlv.kt
@@ -13,4 +13,17 @@ object Tlv {
 
     fun toInt32(bytes: ByteArray): Int {
         require(bytes.size == 4) { "length must be 4, got: ${bytes.size}" }
-        return Byt
+        return ByteBuffer.wrap(bytes, 0, 4).order(ByteOrder.BIG_ENDIAN).int
+    }
+
+    fun parse(data: ByteArray): Map<Byte, List<ByteArray>> {
+        val result = mutableMapOf<Byte, MutableList<ByteArray>>()
+        var rest = data
+        while (rest.isNotEmpty()) {
+            val t = rest[0]
+            val l = rest[1]
+            val v = rest.sliceArray(IntRange(2, (2 + l) - 1))
+            rest = rest.sliceArray(IntRange(2 + l, rest.size - 1))
+            if (v.size < l) continue
+
+            if (!result.conta
