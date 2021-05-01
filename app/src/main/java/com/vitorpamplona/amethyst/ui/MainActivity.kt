984d01b6f964533384b05fc7368e0927dd58d612
vcbit <118248899+vcbit@users.noreply.github.com>
2021-05-01 19:13:32 +0000

    add nuspec

--- app/src/main/java/com/vitorpamplona/amethyst/ui/MainActivity.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/MainActivity.kt
@@ -2,4 +2,17 @@ package com.vitorpamplona.amethyst.ui
 
 import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
-import androidx.activity.c
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.Coil
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.decode.SvgDecoder
+import com.vitorpamplona.amethyst.LocalPreferences
+import com.vitorpamplona.ame
