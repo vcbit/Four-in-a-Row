360d56648504159ff189977cd9f99f63763d8219
vcbit <118248899+vcbit@users.noreply.github.com>
2021-05-01 10:30:13 +0000

    Work toward Python 3.4 support and testing

--- app/src/main/java/com/vitorpamplona/amethyst/service/relays/TypedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/relays/TypedFilter.kt
@@ -11,3 +11,20 @@ class TypedFilter(
 
     fun toJson(): String {
         return GsonBuilder().create().toJson(toJsonObject())
+    }
+
+    fun toJsonObject(): JsonObject {
+        val jsonObject = JsonObject()
+        jsonObject.add("types", typesToJson(types))
+        jsonObject.add("filter", filterToJson(filter))
+        return jsonObject
+    }
+
+    fun typesToJson(types: Set<FeedType>): JsonArray {
+        return JsonArray().apply { types.forEach { add(it.name.lowercase()) } }
+    }
+
+    fun filterToJson(filter: JsonFilter): JsonObject {
+        val jsonObject = JsonObject()
+        filter.ids?.run {
+            jsonObject.ad
