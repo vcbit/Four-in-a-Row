8fe30d6da4e21622ee97ffc5cc71231617d3acd8
vcbit <118248899+vcbit@users.noreply.github.com>
2021-05-09 13:28:48 +0000

    indentation update

--- app/src/main/java/com/vitorpamplona/amethyst/ui/actions/NewChannelViewModel.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/actions/NewChannelViewModel.kt
@@ -8,4 +8,15 @@ import com.vitorpamplona.amethyst.model.Channel
 
 class NewChannelViewModel : ViewModel() {
     private var account: Account? = null
-    private var originalChannel: Channel?
+    private var originalChannel: Channel? = null
+
+    val channelName = mutableStateOf(TextFieldValue())
+    val channelPicture = mutableStateOf(TextFieldValue())
+    val channelDescription = mutableStateOf(TextFieldValue())
+
+    fun load(account: Account, channel: Channel?) {
+        this.account = account
+        if (channel != null) {
+            originalChannel = channel
+            channelName.value = TextFieldValue(channel.info.name ?: "")
+            channelPicture.value = TextFieldValue(channel.info.
