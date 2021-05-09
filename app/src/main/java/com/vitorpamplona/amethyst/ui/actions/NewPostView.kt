be26ddde276b139f005291dd5f917456ce5b0304
vcbit <118248899+vcbit@users.noreply.github.com>
2021-05-09 17:48:24 +0000

    stuf

--- app/src/main/java/com/vitorpamplona/amethyst/ui/actions/NewPostView.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/actions/NewPostView.kt
@@ -1,1 +1,13 @@
 package com.vitorpamplona.amethyst.ui.actions
+
+import android.widget.Toast
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.run
