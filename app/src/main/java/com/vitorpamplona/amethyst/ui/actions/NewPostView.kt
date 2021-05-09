4c860d4257d400e6b6c83562a12efe42b2fcfdef
vcbit <118248899+vcbit@users.noreply.github.com>
2021-05-09 20:55:06 +0000

    Fix identation

--- app/src/main/java/com/vitorpamplona/amethyst/ui/actions/NewPostView.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/actions/NewPostView.kt
@@ -10,4 +10,16 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.run
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+im
