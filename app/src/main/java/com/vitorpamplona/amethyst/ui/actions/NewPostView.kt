d65e083ae22e57b9d5264a4fe2dbb10019ab899e
vcbit <118248899+vcbit@users.noreply.github.com>
2021-06-07 20:50:51 +0000

    update profiles

--- app/src/main/java/com/vitorpamplona/amethyst/ui/actions/NewPostView.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/actions/NewPostView.kt
@@ -270,4 +270,18 @@ fun PostButton(onPost: () -> Unit = {}, isActive: Boolean, modifier: Modifier = 
 }
 
 @Composable
-fun SaveButton(onPost: 
+fun SaveButton(onPost: () -> Unit = {}, isActive: Boolean, modifier: Modifier = Modifier) {
+    Button(
+        modifier = modifier,
+        onClick = {
+            if (isActive) {
+                onPost()
+            }
+        },
+        shape = RoundedCornerShape(20.dp),
+        colors = ButtonDefaults
+            .buttonColors(
+                backgroundColor = if (isActive) MaterialTheme.colors.primary else Color.Gray
+            )
+    ) {
+        Text(text = stringResource(R.s
