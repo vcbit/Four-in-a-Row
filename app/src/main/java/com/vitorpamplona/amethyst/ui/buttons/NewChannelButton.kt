b5d5cdde7d755864ae0edecb4a3c2f2101443932
vcbit <118248899+vcbit@users.noreply.github.com>
2021-06-18 20:39:05 +0000

    gem dump

--- app/src/main/java/com/vitorpamplona/amethyst/ui/buttons/NewChannelButton.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/buttons/NewChannelButton.kt
@@ -9,4 +9,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
-import androidx.co
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.vitorpamplona.amethyst.R
+import com.vitorpamplona.amethyst.model.Account
+import com.vitorpamplona.amethyst.ui.actions.NewChannelView
+
+@Composa
