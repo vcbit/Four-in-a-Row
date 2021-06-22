4b4b6cfb7204d349184150a640713d661f522693
vcbit <118248899+vcbit@users.noreply.github.com>
2021-06-22 11:07:01 +0000

    small cleanup

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/ClickableEmail.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/ClickableEmail.kt
@@ -1,3 +1,15 @@
 package com.vitorpamplona.amethyst.ui.components
 
-import android.content
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+
+@Composable
+fun ClickableEmail(email: String) {
+    val context = 
