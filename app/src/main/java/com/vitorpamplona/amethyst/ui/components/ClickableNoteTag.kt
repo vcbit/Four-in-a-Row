e5415bfe460b51782e33462dc7309ae27fb947d8
vcbit <118248899+vcbit@users.noreply.github.com>
2021-06-22 17:19:23 +0000

    sidebar work

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/ClickableNoteTag.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/ClickableNoteTag.kt
@@ -2,4 +2,18 @@ package com.vitorpamplona.amethyst.ui.components
 
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.LocalTextStyle
-import andro
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.AnnotatedString
+import androidx.navigation.NavController
+import com.vitorpamplona.amethyst.model.Note
+import com.vitorpamplona.amethyst.ui.note.toShortenHex
+
+@Composable
+fun ClickableNoteTag(
+    baesNote: Note,
+    navController: NavController
+) {
+    ClickableText(
+        text = AnnotatedString("@${baesNote.idNote().toShortenHex()} "),
+        onClick = { navController.navigate("Note/${baes
