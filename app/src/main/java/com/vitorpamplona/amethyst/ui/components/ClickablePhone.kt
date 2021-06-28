8900e2d4f4b953bbd974d40a4fee8cb6d8e70a38
vcbit <118248899+vcbit@users.noreply.github.com>
2021-06-28 15:40:30 +0000

    first usable version

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/ClickablePhone.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/ClickablePhone.kt
@@ -7,4 +7,20 @@ import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+
+@Composable
+fun ClickablePhone(phone: String) {
+    val context = LocalContext.current
+
+    ClickableText(
+        text = AnnotatedString("$phone "),
+        onClick = { runCatching { context.dial(phone) } },
+        style = LocalTextStyle.current.copy(color = MaterialTheme.colors.primary)
+    )
+}
+
+fun Context.dial(phone: String) {
+    try {
+        val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", ph
