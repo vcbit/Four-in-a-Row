f9e274b0b8ce9613ba9e999136f27cce17d4f0e6
vcbit <118248899+vcbit@users.noreply.github.com>
2021-06-29 18:01:43 +0000

    Fix webpack config

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/ClickableRoute.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/ClickableRoute.kt
@@ -6,4 +6,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.c
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.text.AnnotatedString
+import androidx.navigation.NavController
+import com.vitorpamplona.amethyst.model.LocalCache
+import com.vitorpamplona.amethyst.service.model.ChannelCreateEvent
+import com.vitorpamplona.amethyst.service.nip19.Nip19
+
+@Composable
+fun ClickableRoute(
+    nip19: Nip19.Return,
+    navController: NavController
+) {
+    if (nip19.type == Nip19.Type.USER) {
+        val userBase = LocalCach
