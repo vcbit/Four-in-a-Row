f6b3aeb9b2c228adb1da0235761aaaf98d60c462
vcbit <118248899+vcbit@users.noreply.github.com>
2021-06-30 16:37:39 +0000

    Localisation

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/ClickableUrl.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/ClickableUrl.kt
@@ -1,3 +1,16 @@
 package com.vitorpamplona.amethyst.ui.components
 
-import android
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.AnnotatedString
+
+@Composable
+fun ClickableUrl(urlText: String, url: String) {
+    val uri = LocalUriHandler.current
+
+    ClickableText(
+        text = AnnotatedString("$urlText "),
+        onClick = { runCatching {
