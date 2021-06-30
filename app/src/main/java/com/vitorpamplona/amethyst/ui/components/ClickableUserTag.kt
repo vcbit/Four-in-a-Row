1f75454884ac7fbd4b8a1bd037f13611769bd1d4
vcbit <118248899+vcbit@users.noreply.github.com>
2021-06-30 21:49:03 +0000

    UPSTREAM: proc: actually make proc_fd_permission() thread-friendly

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/ClickableUserTag.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/ClickableUserTag.kt
@@ -6,4 +6,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui
+import androidx.compose.ui.text.AnnotatedString
+import androidx.navigation.NavController
+import com.vitorpamplona.amethyst.model.User
+
+@Composable
+fun ClickableUserTag(
+    user: User,
+    navController: NavController
+) {
+    val innerUserState by user.live().metadata.observeAsState()
+    ClickableText(
+        text = AnnotatedString("@${innerUserState?.user?.toBestDisplayName()} "),
+        onClick = { navController.navigate("User/${innerUserState?.user?.pubkeyHex}") },
+        style = LocalTextStyle
