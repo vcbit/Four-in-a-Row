b659a94563d4dafdca938aacdf7e6607a661ed10
vcbit <118248899+vcbit@users.noreply.github.com>
2021-07-04 11:54:20 +0000

    added location

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/InvoicePreview.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/InvoicePreview.kt
@@ -4,4 +4,14 @@ import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.lay
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
