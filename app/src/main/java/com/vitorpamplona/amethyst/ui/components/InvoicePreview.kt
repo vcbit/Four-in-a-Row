cb93eb46a84c6a7b9906ebeb1594bc2c20aaa3cc
vcbit <118248899+vcbit@users.noreply.github.com>
2021-07-04 18:53:44 +0000

    homepage tweaks

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/InvoicePreview.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/InvoicePreview.kt
@@ -15,3 +15,15 @@ import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.c
