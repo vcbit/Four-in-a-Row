c402fa0dc0f8b5545cc010feab7cbaa51d2717ff
vcbit <118248899+vcbit@users.noreply.github.com>
2021-07-12 10:42:37 +0000

    organizing f2_reader to convert to fu2 code

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/RobohashAsyncImage.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/RobohashAsyncImage.kt
@@ -4,4 +4,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
-import andr
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+
+@Composable
+fun RobohashAsyncImage(
+    robot: String,
+    modifier: Modifier = Modifier,
+    co
