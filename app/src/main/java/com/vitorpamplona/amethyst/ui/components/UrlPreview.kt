094f593a64c3a758663c884ec9aea27aa232fc8d
vcbit <118248899+vcbit@users.noreply.github.com>
2021-07-15 12:24:18 +0000

    Create constants extension

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/UrlPreview.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/UrlPreview.kt
@@ -4,4 +4,18 @@ import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.ge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import com.baha.url.preview.IUrlPreviewCallback
+import com.baha.url.preview.UrlInfoItem
+import com.vitorpamplona.amethyst.R
+import com.vitorpamplona.amethyst.model.UrlCachedPreviewer
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+@Composable
+fun UrlPreview(url: String, urlText: String) {
+//    val def
