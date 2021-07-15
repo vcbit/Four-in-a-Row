673e485ebcef72f4ad9b7f4befff9fdb1720cb18
vcbit <118248899+vcbit@users.noreply.github.com>
2021-07-15 11:41:54 +0000

    b43: fix frequency reported on G-PHY with /new/ firmware

---
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/UrlPreview.kt
@@ -0,0 +1,7 @@
+package com.vitorpamplona.amethyst.ui.components
+
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.ge
