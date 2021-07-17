80251cec8a82a0aeb274bcce1dd23182a8523f47
vcbit <118248899+vcbit@users.noreply.github.com>
2021-07-17 13:55:12 +0000

    Fix that bug where things didn't work but now they should

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/VideoView.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/VideoView.kt
@@ -7,4 +7,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.p
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
+import com.google.android.exoplayer2.ui.StyledPlayerView
+import com.vitorpamplona.amethyst.VideoCache
+
+@
