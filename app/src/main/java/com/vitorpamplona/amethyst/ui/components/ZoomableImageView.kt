d0deb9828586aa84e7350af9921b602f2803c08b
vcbit <118248899+vcbit@users.noreply.github.com>
2021-07-23 06:18:26 +0000

    add copyright notice

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/ZoomableImageView.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/ZoomableImageView.kt
@@ -1,1 +1,12 @@
-package com.vitorpamplona.amethyst.ui.c
+package com.vitorpamplona.amethyst.ui.components
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.border
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.fou
