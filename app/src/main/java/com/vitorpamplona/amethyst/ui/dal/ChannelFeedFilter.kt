5271b6c45ef8e05bd56f87bc979f9f814665b293
vcbit <118248899+vcbit@users.noreply.github.com>
2021-07-30 08:52:43 +0000

    Cleanup and refactoring

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/ChannelFeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/ChannelFeedFilter.kt
@@ -3,4 +3,19 @@ package com.vitorpamplona.amethyst.ui.dal
 import com.vitorpamplona.amethyst.model.Account
 import com.vitorpamplona.amethyst.model.Channel
 import com.vitorpamplona.amethyst.model.LocalCache
-import com.v
+import com.vitorpamplona.amethyst.model.Note
+
+object ChannelFeedFilter : FeedFilter<Note>() {
+    lateinit var account: Account
+    lateinit var channel: Channel
+
+    fun loadMessagesBetween(accountLoggedIn: Account, channelId: String) {
+        account = accountLoggedIn
+        channel = LocalCache.getOrCreateChannel(channelId)
+    }
+
+    // returns the last Note of each user.
+    override fun feed(): List<Note> {
+        return channel.notes
+            .values
+            .filter { 
