32a2e641d964da70229573a80c40391ed58460ef
vcbit <118248899+vcbit@users.noreply.github.com>
2021-08-05 10:17:30 +0000

    fix oe

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/ChatroomListNewFeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/ChatroomListNewFeedFilter.kt
@@ -3,4 +3,17 @@ package com.vitorpamplona.amethyst.ui.dal
 import com.vitorpamplona.amethyst.model.Account
 import com.vitorpamplona.amethyst.model.Note
 
-object ChatroomListNewFeedFilter : FeedFilter<
+object ChatroomListNewFeedFilter : FeedFilter<Note>() {
+    lateinit var account: Account
+
+    // returns the last Note of each user.
+    override fun feed(): List<Note> {
+        val me = ChatroomListKnownFeedFilter.account.userProfile()
+
+        val privateChatrooms = account.userProfile().privateChatrooms
+        val messagingWith = privateChatrooms.keys.filter {
+            !me.hasSentMessagesTo(it) && account.isAcceptable(it)
+        }
+
+        val privateMessages = messagingWith.mapNotNull { it ->
+            privateCha
