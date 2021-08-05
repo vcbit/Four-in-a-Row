46db5e414a096e78a0a85c83a95b3241edf4eeb8
vcbit <118248899+vcbit@users.noreply.github.com>
2021-08-05 21:28:16 +0000

    Fixes #28

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/FeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/FeedFilter.kt
@@ -6,4 +6,14 @@ import kotlin.time.measureTimedValue
 
 abstract class FeedFilter<T>() {
     @OptIn(ExperimentalTime::class)
-    fun loadTop(): List<T> 
+    fun loadTop(): List<T> {
+        val (feed, elapsed) = measureTimedValue {
+            feed().take(1000)
+        }
+
+        Log.d("Time", "${this.javaClass.simpleName} Feed in $elapsed with ${feed.size} objects")
+        return feed
+    }
+
+    abstract fun feed(): List<T>
+}
