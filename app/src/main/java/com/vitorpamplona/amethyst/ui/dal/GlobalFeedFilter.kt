0efab04853a9f50adc106ac562a20b373da69548
vcbit <118248899+vcbit@users.noreply.github.com>
2021-08-06 11:48:05 +0000

    Run scan-local.sh on chronograf:...

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/GlobalFeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/GlobalFeedFilter.kt
@@ -3,4 +3,14 @@ package com.vitorpamplona.amethyst.ui.dal
 import com.vitorpamplona.amethyst.model.Account
 import com.vitorpamplona.amethyst.model.LocalCache
 import com.vitorpamplona.amethyst.model.Note
-import com.vitorpamplona.ameth
+import com.vitorpamplona.amethyst.service.model.ChannelMessageEvent
+import com.vitorpamplona.amethyst.service.model.LongTextNoteEvent
+import com.vitorpamplona.amethyst.service.model.TextNoteEvent
+
+object GlobalFeedFilter : FeedFilter<Note>() {
+    lateinit var account: Account
+
+    override fun feed() = LocalCache.notes.values
+        .filter {
+            (it.event is TextNoteEvent || it.event is LongTextNoteEvent || it.event is ChannelMessageEvent) &&
+                it.replyTo.isNullOrEmpt
