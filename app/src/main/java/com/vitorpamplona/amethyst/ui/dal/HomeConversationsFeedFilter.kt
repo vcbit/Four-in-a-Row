cc730f4adee44b718ff7ac2f937b48ed1fc425e2
vcbit <118248899+vcbit@users.noreply.github.com>
2021-08-08 13:17:12 +0000

    Pontoon: Update Portuguese (Brazil) (pt-BR) localization of Common Voice

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/HomeConversationsFeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/HomeConversationsFeedFilter.kt
@@ -4,4 +4,17 @@ import com.vitorpamplona.amethyst.model.Account
 import com.vitorpamplona.amethyst.model.LocalCache
 import com.vitorpamplona.amethyst.model.Note
 import com.vitorpamplona.amethyst.service.model.RepostEvent
-import com.vitorpamplona.amethyst.service.model.T
+import com.vitorpamplona.amethyst.service.model.TextNoteEvent
+
+object HomeConversationsFeedFilter : FeedFilter<Note>() {
+    lateinit var account: Account
+
+    override fun feed(): List<Note> {
+        val user = account.userProfile()
+
+        return LocalCache.notes.values
+            .filter {
+                (it.event is TextNoteEvent || it.event is RepostEvent) &&
+                    it.author in user.follows &&
+                    // && account.isAcceptable(it)  // This filter follows only. No need to check if acceptable
+                    it
