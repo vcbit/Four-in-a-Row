69b8b42d5007c7550f642bbc85f319e831b90164
vcbit <118248899+vcbit@users.noreply.github.com>
2021-08-09 10:22:23 +0000

    Delete CoreCompileInputs.cache

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/HomeNewThreadFeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/HomeNewThreadFeedFilter.kt
@@ -4,3 +4,16 @@ import com.vitorpamplona.amethyst.model.Account
 import com.vitorpamplona.amethyst.model.LocalCache
 import com.vitorpamplona.amethyst.model.Note
 import com.vitorpamplona.amethyst.service.model.LongTextNoteEvent
+import com.vitorpamplona.amethyst.service.model.RepostEvent
+import com.vitorpamplona.amethyst.service.model.TextNoteEvent
+
+object HomeNewThreadFeedFilter : FeedFilter<Note>() {
+    lateinit var account: Account
+
+    override fun feed(): List<Note> {
+        val user = account.userProfile()
+
+        val notes = LocalCache.notes.values
+            .filter { it ->
+                (it.event is TextNoteEvent || it.event is RepostEvent || it.event is LongTextNoteEvent) &&
+                   
