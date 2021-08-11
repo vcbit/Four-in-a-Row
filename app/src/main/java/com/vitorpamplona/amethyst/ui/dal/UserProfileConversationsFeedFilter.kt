8d07946de85cf08ea0953a78bdedf7eeb8ac1a9b
vcbit <118248899+vcbit@users.noreply.github.com>
2021-08-11 13:32:16 +0000

    Translation updated by Wouter Bolsterlee.

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/UserProfileConversationsFeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/UserProfileConversationsFeedFilter.kt
@@ -1,1 +1,14 @@
-pack
+package com.vitorpamplona.amethyst.ui.dal
+
+import com.vitorpamplona.amethyst.model.Account
+import com.vitorpamplona.amethyst.model.LocalCache
+import com.vitorpamplona.amethyst.model.Note
+import com.vitorpamplona.amethyst.model.User
+
+object UserProfileConversationsFeedFilter : FeedFilter<Note>() {
+    var account: Account? = null
+    var user: User? = null
+
+    fun loadUserProfile(accountLoggedIn: Account, userId: String) {
+        account = accountLoggedIn
+        user = LocalCache.checkGetOrCr
