e08b98c9766215531737933e72a24bf4b4780e67
vcbit <118248899+vcbit@users.noreply.github.com>
2021-08-14 12:44:55 +0000

    Update References.md

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/UserProfileNewThreadFeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/UserProfileNewThreadFeedFilter.kt
@@ -2,4 +2,19 @@ package com.vitorpamplona.amethyst.ui.dal
 
 import com.vitorpamplona.amethyst.model.Account
 import com.vitorpamplona.amethyst.model.LocalCache
-import com.vitorpamplona.amethyst.model.N
+import com.vitorpamplona.amethyst.model.Note
+import com.vitorpamplona.amethyst.model.User
+
+object UserProfileNewThreadFeedFilter : FeedFilter<Note>() {
+    var account: Account? = null
+    var user: User? = null
+
+    fun loadUserProfile(accountLoggedIn: Account, userId: String) {
+        account = accountLoggedIn
+        user = LocalCache.checkGetOrCreateUser(userId)
+    }
+
+    override fun feed(): List<Note> {
+        val longFormNotes = LocalCache.addressables.values.filter { it.author == user }
+
+        re
