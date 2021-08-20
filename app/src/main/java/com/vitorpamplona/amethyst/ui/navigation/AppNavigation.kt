f4d11e8333a828936c576894976e33e90691b1f5
vcbit <118248899+vcbit@users.noreply.github.com>
2021-08-20 19:40:07 +0000

    Improved debugging

--- app/src/main/java/com/vitorpamplona/amethyst/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/navigation/AppNavigation.kt
@@ -7,4 +7,11 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.rememberPagerState
+import com.vitorpamplona.amethyst.ui.dal.GlobalFeedFilter
+import com.vitorpamplona.amethyst.ui.dal.HomeConversationsFeedFilter
+import com.vitorpamplona.amethyst.ui.dal.HomeNewThreadFeedFilter
+import com.vitorpamplona.amethyst.ui.screen.NostrGlobalFeedViewModel
+import com.vitorpamplona.amethyst.ui.screen.NostrHomeFeedViewModel
+import com.vitorpamplona
