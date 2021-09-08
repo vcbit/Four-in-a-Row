6fad3946e603ff6397fcdcbeb0e1ca757a021bbe
vcbit <118248899+vcbit@users.noreply.github.com>
2021-09-08 12:29:14 +0000

    Added index

--- app/src/main/java/com/vitorpamplona/amethyst/ui/navigation/DrawerContent.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/navigation/DrawerContent.kt
@@ -8,4 +8,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import 
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.mate
