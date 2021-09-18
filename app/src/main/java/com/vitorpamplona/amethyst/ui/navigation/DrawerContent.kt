a63b93bb0568b6bb8dc4cb0b80a342ff8276fc52
vcbit <118248899+vcbit@users.noreply.github.com>
2021-09-18 09:54:15 +0000

    Remove extra newline.

--- app/src/main/java/com/vitorpamplona/amethyst/ui/navigation/DrawerContent.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/navigation/DrawerContent.kt
@@ -199,3 +199,20 @@ fun ProfileContent(baseAccountUser: User, modifier: Modifier = Modifier, scaffol
                 Row() {
                     Text("${accountUserFollows.follows.size}", fontWeight = FontWeight.Bold)
                     Text(stringResource(R.string.following))
+                }
+                Row(modifier = Modifier.padding(start = 10.dp)) {
+                    Text("${accountUserFollows.followers.size}", fontWeight = FontWeight.Bold)
+                    Text(stringResource(R.string.followers))
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ListContent(
+    accountUser: User?,
+    navController: NavHostController,
+    scaffoldState: ScaffoldState,
+    sh
