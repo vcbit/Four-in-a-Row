1f972fcf82f2eaa4fb8759ccfbbd1af3651d8c5b
vcbit <118248899+vcbit@users.noreply.github.com>
2021-09-26 20:27:29 +0000

    missing dep

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/BlankNote.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/BlankNote.kt
@@ -9,4 +9,15 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
-import androidx
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.google.accompanist.flowlayout.FlowRow
+import com.vitorpamplona.amethyst.R
+import com.vit
