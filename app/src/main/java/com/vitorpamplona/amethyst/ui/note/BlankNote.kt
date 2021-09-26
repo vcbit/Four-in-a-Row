1c42b9ecedc5a63ea77781568e3aa84863e3fe60
vcbit <118248899+vcbit@users.noreply.github.com>
2021-09-26 17:13:13 +0000

    Update new-age.css

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/BlankNote.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/BlankNote.kt
@@ -1,1 +1,12 @@
-package com.vito
+package com.vitorpamplona.amethyst.ui.note
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Divider
+import androidx
