2df7421759a8868294fc56bf20bcf17b330ffa15
vcbit <118248899+vcbit@users.noreply.github.com>
2021-10-01 22:42:26 +0000

    f2fs: avoid needless lock for node pages when fsyncing a file

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/ChatroomCompose.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/ChatroomCompose.kt
@@ -3,4 +3,15 @@ package com.vitorpamplona.amethyst.ui.note
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layou
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Divider
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.
