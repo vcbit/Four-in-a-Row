726f4066bee3534f3cd9d00f6db013b67c0e0a30
vcbit <118248899+vcbit@users.noreply.github.com>
2021-10-27 14:32:07 +0000

    release 0.0.3

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/ChatroomMessageCompose.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/ChatroomMessageCompose.kt
@@ -228,4 +228,11 @@ fun ChatroomMessageCompose(
                             if (!innerQuote && !replyTo.isNullOrEmpty()) {
                                 Row(verticalAlignment = Alignment.CenterVertically) {
                                     replyTo.toSet().mapIndexed { _, note ->
-                                        if (note.event != 
+                                        if (note.event != null) {
+                                            ChatroomMessageCompose(
+                                                note,
+                                                null,
+                                                innerQuote = true,
+                                                parentBackgroundColor = backgroundBubbleColor,
+                                                accountViewModel = accountViewModel,
+                                                navController =
