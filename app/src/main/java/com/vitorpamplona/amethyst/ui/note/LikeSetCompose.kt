449b03d731b1b845677118cb18d1cae2240db442
vcbit <118248899+vcbit@users.noreply.github.com>
2021-11-06 08:51:15 +0000

    Update bundles

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/LikeSetCompose.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/LikeSetCompose.kt
@@ -15,4 +15,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.com
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.google.accompanist.flowlayout.FlowRow
+import com.vitorpamplona.
