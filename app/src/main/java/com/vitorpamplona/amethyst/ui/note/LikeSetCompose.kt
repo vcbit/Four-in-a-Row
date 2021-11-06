ffd172ca28628d8fbe3f4e512c4f66a82b83ff93
vcbit <118248899+vcbit@users.noreply.github.com>
2021-11-06 08:27:18 +0000

    add ad

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/LikeSetCompose.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/LikeSetCompose.kt
@@ -4,4 +4,15 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layou
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.com
