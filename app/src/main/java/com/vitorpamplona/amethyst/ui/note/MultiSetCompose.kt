e165f35b386a754cd70eb1d1630cdbfaee225e6d
vcbit <118248899+vcbit@users.noreply.github.com>
2021-11-13 11:58:26 +0000

    update puppy

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/MultiSetCompose.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/MultiSetCompose.kt
@@ -8,4 +8,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bolt
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import 
