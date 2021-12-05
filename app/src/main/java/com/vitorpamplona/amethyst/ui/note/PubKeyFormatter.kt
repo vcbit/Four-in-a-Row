ad6e03d358181a164575e36bb11950395bd91149
vcbit <118248899+vcbit@users.noreply.github.com>
2021-12-05 20:30:15 +0000

    ath9k_htc: Restore skb headroom when returning skb to mac80211

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/PubKeyFormatter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/PubKeyFormatter.kt
@@ -1,3 +1,11 @@
 package com.vitorpamplona.amethyst.ui.note
 
-import nostr.postr.t
+import nostr.postr.toHex
+
+fun ByteArray.toShortenHex(): String {
+    return toHex().toShortenHex()
+}
+
+fun String.toShortenHex(): String {
+    return replaceRange(8, length - 8, ":")
+}
---
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/ReactionsRow.kt
@@ -0,0 +1,7 @@
+package com.vitorpamplona.amethyst.ui.note
+
+import android.widget.Toast
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.Mu
