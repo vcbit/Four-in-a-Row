a2362756788daec6d5f34daebf6439308b194d02
vcbit <118248899+vcbit@users.noreply.github.com>
2021-12-07 17:43:48 +0000

    Added installation instructions.

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/ReactionsRow.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/ReactionsRow.kt
@@ -4,4 +4,13 @@ import android.widget.Toast
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.interaction.Mu
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose
