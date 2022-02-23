1caa7ce5f9f82c0315cb6b058b8213f753979e13
vcbit <118248899+vcbit@users.noreply.github.com>
2022-02-23 10:35:03 +0000

    gitignore again

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/TimeAgoFormatter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/TimeAgoFormatter.kt
@@ -1,4 +1,17 @@
 package com.vitorpamplona.amethyst.ui.note
 
 import android.content.Context
-import android.te
+import android.text.format.DateUtils
+import com.vitorpamplona.amethyst.R
+
+fun timeAgo(mills: Long?, context: Context): String {
+    if (mills == null) return " "
+    if (mills == 0L) return " • ${context.getString(R.string.never)}"
+
+    var humanReadable = DateUtils.getRelativeTimeSpanString(
+        mills * 1000,
+        System.currentTimeMillis(),
+        DateUtils.MINUTE_IN_MILLIS,
+        DateUtils.FORMAT_ABBREV_ALL
+    ).toString()
+    if (humanReadable.startsWith("In") || humanReadable.startsW
