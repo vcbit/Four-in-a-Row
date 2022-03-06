e39c3d37f180803921ff0085c1d7e0ba669cdd8f
vcbit <118248899+vcbit@users.noreply.github.com>
2022-03-06 18:02:56 +0000

    vundefined

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/UserCompose.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/UserCompose.kt
@@ -3,4 +3,15 @@ package com.vitorpamplona.amethyst.ui.note
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compos
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
