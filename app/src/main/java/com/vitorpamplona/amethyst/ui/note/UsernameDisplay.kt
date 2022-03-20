0db1755dc641b5eae5bc79a8ea819b3f088673a8
vcbit <118248899+vcbit@users.noreply.github.com>
2022-03-20 20:53:08 +0000

    BACKPORT: tty: Prevent ldisc drivers from re-using stale tty fields

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/UsernameDisplay.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/UsernameDisplay.kt
@@ -4,4 +4,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.obser
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import com.vitorpamplona.amethyst.model.Note
+import com.vitorpamplona.amethyst.model.User
+
+@Composable
+fun NoteUsernameDisplay(baseNote: Note, weight: Modifier = Modifier) {
+    val noteState by baseNote.live().metadata.observeAsState()
+    val note = noteState?.note ?: return
+
+    val author = note.author
+
+    if (author !
