62f22921a4151e1b415aac5d8eeceefeeb7127e2
vcbit <118248899+vcbit@users.noreply.github.com>
2022-03-27 21:45:42 +0000

    Fix a few things

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/ZapNoteCompose.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/ZapNoteCompose.kt
@@ -1,1 +1,13 @@
-package com.vitorpamplona.amethyst.ui.
+package com.vitorpamplona.amethyst.ui.note
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compos
