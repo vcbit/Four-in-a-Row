1027b7c76137909e277ba69c4469f45859f46b42
vcbit <118248899+vcbit@users.noreply.github.com>
2022-04-24 21:02:43 +0000

    Bump version to 0.7.0

--- app/src/main/java/com/vitorpamplona/amethyst/ui/screen/AccountScreen.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/screen/AccountScreen.kt
@@ -1,1 +1,14 @@
-package com.vitorpamplona.amet
+package com.vitorpamplona.amethyst.ui.screen
+
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.vitorpamplona.amethyst.ui.screen.loggedIn.AccountViewModel
+import com.vitorpamplona.amethyst.ui.screen.loggedIn.MainScreen
+import com.vitorpamplona.amethyst.ui.screen.loggedOff.LoginPage
+
+@Composable
+fun AccountScr
