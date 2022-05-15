5d7ea2184a29bf933711a5cbc34a4c40b7f053a9
vcbit <118248899+vcbit@users.noreply.github.com>
2022-05-15 11:30:51 +0000

    Fix linting problems

--- app/src/main/java/com/vitorpamplona/amethyst/ui/screen/AccountScreen.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/screen/AccountScreen.kt
@@ -11,4 +11,13 @@ import com.vitorpamplona.amethyst.ui.screen.loggedIn.MainScreen
 import com.vitorpamplona.amethyst.ui.screen.loggedOff.LoginPage
 
 @Composable
-fun AccountScr
+fun AccountScreen(accountStateViewModel: AccountStateViewModel, startingPage: String?) {
+    val accountState by accountStateViewModel.accountContent.collectAsState()
+
+    Column() {
+        Crossfade(targetState = accountState, animationSpec = tween(durationMillis = 100)) { state ->
+            when (state) {
+                is AccountState.LoggedOff -> {
+                    LoginPage(accountStateViewModel, isFirstLogin = true)
+                }
+                is AccountState.Logg
