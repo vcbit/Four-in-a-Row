cdd836303e048b3677037e012bb7ae1a97f927f9
vcbit <118248899+vcbit@users.noreply.github.com>
2022-05-21 15:18:36 +0000

    investigation

--- app/src/main/java/com/vitorpamplona/amethyst/ui/screen/AccountStateViewModel.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/screen/AccountStateViewModel.kt
@@ -14,4 +14,16 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import nostr.postr.Perso
+import nostr.postr.Persona
+import nostr.postr.bechToBytes
+import java.util.regex.Pattern
+
+class AccountStateViewModel() : ViewModel() {
+    private val _accountContent = MutableStateFlow<AccountState>(AccountState.LoggedOff)
+    val accountContent = _accountContent.asStateFlow()
+
+    init {
+        // pulls account from storage.
+
+        // Keeps it in the the UI thread to void blinking the login page.
+        // viewModelScope.launch(Dispatchers.IO
