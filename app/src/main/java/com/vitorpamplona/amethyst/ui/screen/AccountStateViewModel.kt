9567dd53234d8420a4a7867ee45f6304c76836a8
vcbit <118248899+vcbit@users.noreply.github.com>
2022-05-21 13:33:37 +0000

    more TODOs

--- app/src/main/java/com/vitorpamplona/amethyst/ui/screen/AccountStateViewModel.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/screen/AccountStateViewModel.kt
@@ -1,4 +1,17 @@
 package com.vitorpamplona.amethyst.ui.screen
 
 import androidx.lifecycle.ViewModel
-import com.vitorpamplona.amethyst.Loca
+import com.vitorpamplona.amethyst.LocalPreferences
+import com.vitorpamplona.amethyst.ServiceManager
+import com.vitorpamplona.amethyst.model.Account
+import fr.acinq.secp256k1.Hex
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import nostr.postr.Perso
