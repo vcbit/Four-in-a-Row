c843bed80625b88a8e37140d1ae88355bd790fd3
vcbit <118248899+vcbit@users.noreply.github.com>
2022-05-22 21:40:22 +0000

    tcp: must unclone packets before mangling them

--- app/src/main/java/com/vitorpamplona/amethyst/ui/screen/CardFeedState.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/screen/CardFeedState.kt
@@ -1,3 +1,24 @@
 package com.vitorpamplona.amethyst.ui.screen
 
-import androidx.compose.
+import androidx.compose.runtime.MutableState
+import com.vitorpamplona.amethyst.model.Note
+
+abstract class Card() {
+    abstract fun createdAt(): Long
+    abstract fun id(): String
+}
+
+class BadgeCard(val note: Note) : Card() {
+    override fun createdAt(): Long {
+        return note.createdAt() ?: 0
+    }
+
+    override fun id() = note.idHex
+}
+
+class NoteCard(val note: Note) : Card() {
+    override fun createdAt(): Long {
+        return note.createdAt() ?: 0
+    }
+
+    override fun id() 
