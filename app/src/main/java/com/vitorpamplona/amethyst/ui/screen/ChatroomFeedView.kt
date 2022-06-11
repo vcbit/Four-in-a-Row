18919990928d0ff82ce0230aef0bdb7b28a08d8f
vcbit <118248899+vcbit@users.noreply.github.com>
2022-06-11 19:20:37 +0000

    Improving readme.

--- app/src/main/java/com/vitorpamplona/amethyst/ui/screen/ChatroomFeedView.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/screen/ChatroomFeedView.kt
@@ -1,1 +1,12 @@
-package com.
+package com.vitorpamplona.amethyst.ui.screen
+
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValu
