ed9fb394b52b1f5bfdc0f6eeb93136c94abf30ea
vcbit <118248899+vcbit@users.noreply.github.com>
2022-06-18 09:08:05 +0000

    Update version to 0.1.2

--- app/src/main/java/com/vitorpamplona/amethyst/ui/screen/LnZapFeedView.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/screen/LnZapFeedView.kt
@@ -1,3 +1,12 @@
 package com.vitorpamplona.amethyst.ui.screen
 
-import androi
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.
