48034ff309e62be9f783d41d0e6b5dab82f20e4c
vcbit <118248899+vcbit@users.noreply.github.com>
2022-06-18 13:54:51 +0000

    add git

--- app/src/main/java/com/vitorpamplona/amethyst/ui/screen/LnZapFeedView.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/screen/LnZapFeedView.kt
@@ -9,4 +9,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.com
