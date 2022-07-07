e2d2b45cb537f5e8350489f00051276c886e7a60
vcbit <118248899+vcbit@users.noreply.github.com>
2022-07-07 14:45:26 +0000

    Fixed readme link

--- app/src/main/java/com/vitorpamplona/amethyst/ui/screen/RelayFeedView.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/screen/RelayFeedView.kt
@@ -1,1 +1,10 @@
-p
+package com.vitorpamplona.amethyst.ui.screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRef
