1b927dc6a56e0b06e0c8c44c6eafffd54015e57f
vcbit <118248899+vcbit@users.noreply.github.com>
2022-07-09 12:25:02 +0000

    Update components.

--- app/src/main/java/com/vitorpamplona/amethyst/ui/screen/RelayFeedView.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/screen/RelayFeedView.kt
@@ -7,4 +7,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.pullrefresh.PullRef
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compos
