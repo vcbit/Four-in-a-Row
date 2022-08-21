f89229a563d39ada0248e4ab3acb932ed9379dd5
vcbit <118248899+vcbit@users.noreply.github.com>
2022-08-21 23:11:18 +0000

    0.3.11

--- app/src/main/java/com/vitorpamplona/amethyst/ui/screen/loggedOff/LoginScreen.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/screen/loggedOff/LoginScreen.kt
@@ -1,4 +1,14 @@
 package com.vitorpamplona.amethyst.ui.screen.loggedOff
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.l
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Visibility
+import a
