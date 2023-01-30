cdb5d8c9a85083f0c63a907ed0b5be267bb817fd
vcbit <118248899+vcbit@users.noreply.github.com>
2023-01-30 21:01:21 +0000

    Binaries

---
+++ app/src/test/java/com/vitorpamplona/amethyst/KeyParseTest.kt
@@ -0,0 +1,13 @@
+package com.vitorpamplona.amethyst
+
+import com.vitorpamplona.amethyst.model.parseDirtyWordForKey
+import com.vitorpamplona.amethyst.service.nip19.Nip19
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class KeyPars
