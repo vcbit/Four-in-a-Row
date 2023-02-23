d1ef5de7b9859496604af6a385eca934bea48d74
vcbit <118248899+vcbit@users.noreply.github.com>
2023-02-23 07:14:30 +0000

    Fixed #24

--- app/src/test/java/com/vitorpamplona/amethyst/service/zaps/UserZapsTest.kt
+++ app/src/test/java/com/vitorpamplona/amethyst/service/zaps/UserZapsTest.kt
@@ -13,4 +13,23 @@ import java.math.BigDecimal
 class UserZapsTest {
     @Test
     fun nothing() {
-        Assert.assert
+        Assert.assertEquals(1, 1)
+    }
+
+    @Test
+    fun user_without_zaps() {
+        val actual = UserZaps.forProfileFeed(zaps = null)
+
+        Assert.assertEquals(emptyList<Pair<Note, Note>>(), actual)
+    }
+
+    @Test
+    fun avoid_duplicates_with_same_zap_request() {
+        val zapRequest = mockk<Note>()
+
+        val zaps: Map<Note, Note?> = mapOf(
+            zapRequest to mockZapNoteWith("user-1", amount = 100),
+            zapRequest to mockZapNoteWith("user-1", amount = 200)
+        )
+
+        val actual = UserZaps.forProfil
